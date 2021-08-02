133e4fa5af9d1222bba8eb897b6394c56efc5f6f
Luke256 <yuta.k.200628@gmail.com>
2021-08-02 12:59:28 +0900

    無駄な部分の削除

--- Loss.hpp
+++ Loss.hpp
@@ -4,6 +4,6 @@
 namespace Loss{
     long double mean_squared_error(std::vector<std::vector<long double>> &y, std::vector<std::vector<long double>> &t);
     long double mean_cross_entropy_error(std::vector<std::vector<long double>> &y, std::vector<std::vector<long double>> &t);
-    std::vector<std::vector<long double>> mean_squared_error_back(long double x, std::vector<std::vector<long double>>absolute_error);
+    std::vector<std::vector<long double>> mean_squared_error_back( std::vector<std::vector<long double>>absolute_error);
     std::vector<std::vector<long double>> mean_cross_entropy_error_back(std::vector<std::vector<long double>>error);
 };
